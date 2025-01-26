6ae0736019e8f8c195f2e868192913e47586e2d9
Bia <ntenhodoritosmasdepende@gmail.com>
2025-01-26 16:28:21 -0300

    Implementando uma função para atualizar a lista de amigos

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 let amigos = [];
 
+const amigo = document.getElementById("amigo");
+const resultado = document.getElementById("resultado");
+const listaAmigos = document.getElementById("listaAmigos");
+
 function adicionarAmigo(){
-  const amigo = document.getElementById("amigo").value.trim();
-  if(amigo === ''){
+  const novoAmigo = amigo.value.trim();
+  if(novoAmigo === ''){
     resultado.innerHTML = '<p class="error">Por favor, insira um nome.</p>';
   }else{
-    amigos.push(amigo);
     resultado.innerHTML = '';
+    amigo.value = '';
+    amigos.push(novoAmigo);
+    parecerLista(amigos);
+  }
+}
+
+function parecerLista(amigos){
+  listaAmigos.innerHTML = '';
+  console.log(quantidade);
+  for(const amigo of amigos){
+    listaAmigos.innerHTML += `<li>${amigo}</li>`;
   }
 }
